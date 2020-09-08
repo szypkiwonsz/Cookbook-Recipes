87360868c5bc4614dd966e29acd7670c2a24c9e0
szypkiwonsz <kacpersawicki321@gmail.com>
2020-09-08 19:04:48 +0200

    Add base html template
    - add bootstrap css,
    - add scripts

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from recipes import Recipes
 
 app = Flask(__name__)
+r = Recipes
 
 
 @app.route('/')
-def hello_world():
-    return 'hello world'
+def index():
+    recipes = r.get_recipes('https://recipes-cookbook-api.herokuapp.com/api/recipes/')
+    print(recipes)
+    return render_template('index.html', recipes=recipes)
 
 
 if __name__ == '__main__':
