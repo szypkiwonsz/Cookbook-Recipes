586bf53104cacbf14f742a12971168c6b7150edf
szypkiwonsz <kacpersawicki321@gmail.com>
2020-10-15 17:20:59 +0200

    Add flash message for login_required decorator

--- app/decorators.py
+++ app/decorators.py
@@ -1,11 +1,12 @@
 from functools import wraps
-from flask import g, request, redirect, url_for
+from flask import g, request, redirect, url_for, flash
 
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if g.user_token is None:
+            flash('To access this page please sign in.')
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
 
